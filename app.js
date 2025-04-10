b0b5e024725a9824a5397b527a149534fa791009
isander94 <isaac.andersson0015@stud.hkr.se>
2025-04-10 17:51:45 +0200

    defined __filename, __dirname and added root path

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ import dotenv from "dotenv"
 import path from "path"
 import dish from "./models/dish.js"
 import connect from "./config/database.js"
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+// Define __filename and __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename)
 
 dotenv.config()
 
@@ -15,6 +21,16 @@ const app = express()
 
 // Middleware to allow the use of json parsing
 app.use(express.json())
+
+// Middleware for showing static web content
+app.use(express.static(path.join(__dirname, "public")))
+
+// Root route
+app.get("/", async (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"))
+})
+
+// TO-DO: Mount routes
 
 // Define port and start server
 const port = process.env.PORT || 3000
